67adf7a0aaf15203539e1cb362f4ed9875d1eeb3
Robert Loy (GCU) <robert.loy@gcu.edu>
2020-06-15 13:49:02 -0700

    Update Start.java
    Added welcome message

--- src/edu/gcu/cst135n/gitdemo/Start.java
+++ src/edu/gcu/cst135n/gitdemo/Start.java
@@ -3,6 +3,7 @@ package edu.gcu.cst135n.gitdemo;
 public class Start {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to the airport simulator.");
 		Airport skyHarbor = new Airport();
 		skyHarbor.flyPlane();
 	}
