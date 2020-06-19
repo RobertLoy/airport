a62cf526e3b89b91aadf3892700efeef6c20f11a
unknown <ssorden@fusd1.org>
2020-06-18 21:07:10 -0700

    Moved airplae forward 1 mile

--- src/edu/gcu/cst135n/gitdemo/Airplane.java
+++ src/edu/gcu/cst135n/gitdemo/Airplane.java
@@ -3,11 +3,19 @@ public class Airplane {
 	
 	private int altitude = 0;
 	private Pilot pilot;
+	private int distance = 0;
 	
 	public void goUp() {
 		System.out.println("Plane is going up 100 feet");
 		altitude += 100;
 		System.out.println("The plane is now at " + altitude + " feet.");
+	}
+	
+	public void moveForward() {
+		System.out.println("The plan is moving");
+		distance +=1;
+		System.out.println("The plane is at " + distance + " miles."  );
+		
 	}
 	
 	public void addPilot() {
