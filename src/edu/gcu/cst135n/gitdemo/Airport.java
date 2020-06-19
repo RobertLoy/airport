dbd1fbf2e173386071c59490216ca3b3eccb2914
unknown <philip.evans89@gmail.com>
2020-06-18 20:57:18 -0700

    Ticket #4234

--- src/edu/gcu/cst135n/gitdemo/Airport.java
+++ src/edu/gcu/cst135n/gitdemo/Airport.java
@@ -16,9 +16,9 @@ public class Airport {
 		plane.addPilot();
 		System.out.println(plane);
 		do {
-			System.out.println("Do you want the plane to go up? [Y = Yes / N = No / X = eXit] ");
-			option = sc.nextLine();
-			if (option.equals("Y"))
+			System.out.println("Do you want the plane to go up? [Y or Yes / N or No / X = Exit] ");
+			option = (sc.nextLine().toUpperCase());
+			if (option.equals("Y") || option.equals("YES"))
 				plane.goUp();
 		} while (!(option.equals("X")));
 		System.out.println("Thank you for flying with us!");
